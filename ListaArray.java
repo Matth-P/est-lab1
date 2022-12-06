db77971462128e54697a6416f6c4bdc8e2968041
Matth-P <109482342+Matth-P@users.noreply.github.com>
2022-12-06 21:01:37 +0000

    Aula1

--- ListaArray.java
+++ ListaArray.java
@@ -1,48 +1,69 @@
 public class ListaArray implements EstruturaDeDados{
 
+    private int[] elementos;
+    private int contador;
+
+    public ListaArray(){
+        elementos = new int[100];
+        contador = 0;
+
+    }
+
+    public int getElementos (int chave){
+        return elementos[chave];
+    }
+
+
     @Override
     public boolean insert(int chave) {
-        // TODO Auto-generated method stub
-        return false;
+        if (contador > 100){
+            return false;
+        }
+        else{
+            elementos[contador] = chave;
+            contador = contador + 1;
+            return true;
+        }
     }
 
     @Override
     public boolean delete(int chave) {
-        // TODO Auto-generated method stub
-        return false;
     }
 
     @Override
     public boolean search(int chave) {
-        // TODO Auto-generated method stub
+        for (int i = 0; i < 100; i++){
+            if (elementos[i] == chave){
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public int minimum() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public int maximum() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public int sucessor(int chave) {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public int prodessor(int chave) {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     public static void main(String[] args) {
         ListaArray r = new ListaArray();
+        r.insert(1);
+        System.out.println(r.getElementos(0));
+        r.search(1);
     }
 }
